2cf69ca098537b72d303daa3c1695a0ba3b9dc9f
rathodrudra288006-hue <rathodrudra288006@gamil.com>
2026-05-19 16:49:22 +0530

    tuesday commit

---
+++ JS Project-Image Editor/script.js
@@ -0,0 +1,33 @@
+const filters = {
+  brightness: { value: 100, min: 0, max: 200, unit: "%" },
+  contrast: { value: 100, min: 0, max: 200, unit: "%" },
+  exposure: { value: 100, min: 0, max: 200, unit: "%" },
+  saturation: { value: 100, min: 0, max: 200, unit: "%" },
+  hueRotation: { value: 0, min: 0, max: 360, unit: "deg" },
+  blur: { value: 0, min: 0, max: 20, unit: "px" },
+  grayscale: { value: 0, min: 0, max: 100, unit: "%" },
+  sepia: { value: 0, min: 0, max: 100, unit: "%" },
+  opacity: { value: 100, min: 0, max: 100, unit: "%" },
+  invert: { value: 0, min: 0, max: 100, unit: "%" },
+};
+
+function createFilterElement(name, unit = "%", value, min, max) {
+  const div = document.createElement("div");
+  div.classList.add("filter");
+
+  const input = document.createElement("input");
+  input.type = "range";
+  input.min = min;
+  input.max = max;
+  input.value = value;
+  input.id = name;
+
+  const p = document.createElement("p");
+  p.innerText = name;
+
+  div.appendChild(p);
+  div.appendChild(input);
+
+  return div;
+}
+Object.keys(filters).forEach((filterName) => {});
